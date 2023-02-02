f02775da3710d239fded008049e0cc56101846a4
eshamard <eshamard@redhat.com>
2023-02-02 20:19:02 +0100

    Add JWT and authContext
    - put JWT token to cookies for users
    - start using authContext to handle future work

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,6 @@
 import {db} from '../db.js'
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 export const register = (req, res) => {
 
@@ -30,12 +31,18 @@ export const login = (req, res) => {
 
     db.query(query, [req.body.username], (err, data) => {
         if(err) return res.json(err);
-        if(data.length ===0) return res.status(404).json("User not found!")
+        if(data.length ===0) return res.status(404).json("User not found!");
 
-        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password)
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
 
-        if(!isPasswordCorrect) return res.status(401).json("Username or password is wrong!")
-        else return res.status(200).json("Logged in successfully!")
+        if(!isPasswordCorrect) return res.status(401).json("Username or password is wrong!");
+
+        const token = jwt.sign({id : data[0].id}, "jwttoken");
+        const { password, ...other } = data[0];
+
+        res.cookie("access_token", token, {
+            httpOnly: true
+        }).status(200).json(other);
 
     })
 }
